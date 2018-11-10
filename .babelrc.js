60818eea41758722e28b870c2c9dfbc4dda5c9ab
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-11-11 00:41:19 +0100

    Working lib

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+const { NODE_ENV } = process.env
+const test = NODE_ENV === 'test'
+const loose = true
+
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        loose,
+        modules: false,
+      },
+    ],
+  ],
+  plugins: [test && '@babel/transform-modules-commonjs'].filter(Boolean),
+}
